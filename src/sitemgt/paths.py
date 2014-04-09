87f5244474d1179b19cc02091b19a00d9896fe6d
Jody Sankey <jody@jsankey.com>
2014-04-09 06:06:27 +0000

    Add site mount information

--- src/sitemgt/paths.py
+++ src/sitemgt/paths.py
@@ -11,6 +11,7 @@ __author__="Jody"
 __date__ ="$Date:$"
 
 import os
+import subprocess
 
 SITE_BASE_DIR = os.environ["SITEPATH"]
 SITE_XML_FILE =  os.path.join(SITE_BASE_DIR, "svn/site/xml/SiteDescription.xml")
@@ -26,3 +27,25 @@ def getDeploymentFile(host_name):
 def getStatusReportFile(host_name):
     """Return the qualified path name of the deployment record for the specified host"""
     return os.path.join(SITE_BASE_DIR, "status/{}_status_report.xml".format(host_name))
+
+
+# Note these two functions are pretty fragile. Cannot afford to have multiple scripts that 
+# using them in parallel, errors will be thrown up to the caller, and we rely on the environment 
+# variable being mounted directly. All that said the effect of failure is pretty benign.
+
+def mountSiteDir():
+    """Mount the site directory if it is not currently mounted. Returns a token which may
+    be passed to unmountSiteDir to ensure a symmetric response"""
+    mounts = subprocess.check_output(["mount"]).decode("utf-8").split("\n")
+    if len([m for m in mounts if (" " + SITE_BASE_DIR + " ") in m]):
+        # Site path not found in mount table, do the mount - this may fail if not root
+        subprocess.check_output(["mount",SITE_BASE_DIR])
+        return True
+    else:
+        return False
+
+def unmountSiteDir(token): 
+    """Unmount the site directory if the token passed from mountSiteDir indicates a mount 
+    was performed"""
+    if token is True:
+        subprocess.check_output(["umount",SITE_BASE_DIR])
