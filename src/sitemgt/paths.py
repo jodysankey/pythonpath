4af147a817ee8b9fd67a11f33469a3a362ca4298
Jody Sankey <jody@jsankey.com>
2020-04-30 01:05:00 -0700

    Update site paths from svn to git.

--- src/sitemgt/paths.py
+++ src/sitemgt/paths.py
@@ -7,18 +7,17 @@
 # management locations
 #========================================================
 
-__author__="Jody"
-__date__ ="$Date:$"
-
 import os
 import subprocess
 
 SITE_BASE_DIR = os.environ["SITEPATH"]
-SITE_XML_FILE =  os.path.join(SITE_BASE_DIR, "svn/site/xml/SiteDescription.xml")
+
+SITE_XML_FILE =  os.path.join(SITE_BASE_DIR, "repo/site/xml/SiteDescription.xml")
 WEB_OUTPUT_DIR = os.path.join(SITE_BASE_DIR, "website/variable")
 CHECK_RESULTS_DIR = os.path.join(SITE_BASE_DIR, "checks")
-CHECK_SRC_DIR = os.path.join(SITE_BASE_DIR, "svn/site/checks")
-CM_WORKING_DIR = os.path.join(SITE_BASE_DIR, "svn")
+CHECK_SRC_DIR = os.path.join(SITE_BASE_DIR, "repo/site/checks")
+CM_WORKING_DIR = os.path.join(SITE_BASE_DIR, "repo")
+CM_UPSTREAM_DIR = os.path.join(SITE_BASE_DIR, "repo.git")
 
 def getDeploymentFile(host_name):
     """Return the qualified path name of the deployment record for the specified host"""
@@ -28,8 +27,8 @@ def getStatusReportFile(host_name):
     """Return the qualified path name of the deployment record for the specified host"""
     return os.path.join(SITE_BASE_DIR, "status/{}_status_report.xml".format(host_name))
 
-# Note these two functions are pretty fragile. Cannot afford to have multiple scripts that 
-# using them in parallel, errors will be thrown up to the caller, and we rely on the environment 
+# Note these two functions are pretty fragile. Cannot afford to have multiple scripts that
+# using them in parallel, errors will be thrown up to the caller, and we rely on the environment
 # variable being mounted directly. All that said the effect of failure is pretty benign.
 
 def mountSiteDir():
@@ -43,8 +42,8 @@ def mountSiteDir():
     else:
         return False
 
-def unmountSiteDir(token): 
-    """Unmount the site directory if the token passed from mountSiteDir indicates a mount 
+def unmountSiteDir(token):
+    """Unmount the site directory if the token passed from mountSiteDir indicates a mount
     was performed"""
     if token is True:
         subprocess.check_output(["umount",SITE_BASE_DIR])
