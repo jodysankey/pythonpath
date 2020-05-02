29a4a91c0088d0ddd4ada012a0084fe586a409be
Jody Sankey <jody@jsankey.com>
2020-05-02 00:40:39 -0700

    Un-obfuscate private variables.

--- src/tagwriter.py
+++ src/tagwriter.py
@@ -10,41 +10,41 @@ class TagWriter(object):
     """Simplistic class to write XML/HTML style tagged text to a file"""
 
     def __init__(self, filename):
-        self.__f = open(filename, 'w')
-        self.__stack = []
+        self._f = open(filename, 'w')
+        self._stack = []
         self.filename = filename
 
     def __del__(self):
-        self.__f.close()
+        self._f.close()
 
     def write(self, tag, attributes='', text=''):
         """Writes a tag including attributes, of name tag, and containing text."""
-        self.__f.write("<{}".format(tag))
+        self._f.write("<{}".format(tag))
         if attributes != '':
-            self.__f.write(" {}".format(attributes))
-        self.__f.write(">{}</{}>\n".format(text, tag))
+            self._f.write(" {}".format(attributes))
+        self._f.write(">{}</{}>\n".format(text, tag))
 
     def write_orphan(self, tag, attributes):
         """Writes an unclosed tag without adding it to the stack."""
-        self.__f.write("<{} {}>\n".format(tag, attributes))
+        self._f.write("<{} {}>\n".format(tag, attributes))
 
     def write_text(self, text):
         """Writes the supplied text directly."""
-        self.__f.write(text)
+        self._f.write(text)
 
     def open(self, tag, attributes=''):
         """Opens a new nested tag."""
         if attributes == '':
-            self.__f.write("<{}>\n".format(tag))
+            self._f.write("<{}>\n".format(tag))
         else:
-            self.__f.write("<{} {}>\n".format(tag, attributes))
-        self.__stack.append("</{}>\n".format(tag))
+            self._f.write("<{} {}>\n".format(tag, attributes))
+        self._stack.append("</{}>\n".format(tag))
 
     def close(self, count=1):
         """Closes count number of open tags."""
         for _ in range(count): #@UnusedVariable
-            self.__f.write(self.__stack.pop())
+            self._f.write(self._stack.pop())
 
     def depth(self):
         """Returns the number of currenly open tags."""
-        return len(self.__stack)
+        return len(self._stack)
