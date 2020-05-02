920bce2fe7bea838dd4505062ae29a47ad02713a
Jody Sankey <jody@jsankey.com>
2020-05-01 22:44:45 -0700

    Linting on tag writer.

--- src/tagwriter.py
+++ src/tagwriter.py
@@ -3,43 +3,48 @@
 #========================================================
 # PublicPermissions: True
 #========================================================
-# Simple class to output XML/HTML style formatted
-# text with elements and attributes
-#========================================================
 
+"""Simple class to output XML/HTML style formatted text with elements and attributes."""
 
 class TagWriter(object):
     """Simplistic class to write XML/HTML style tagged text to a file"""
 
-    def __init__(self,filename):
-        self.__f = open(filename,'w')
+    def __init__(self, filename):
+        self.__f = open(filename, 'w')
         self.__stack = []
         self.filename = filename
+
     def __del__(self):
         self.__f.close()
 
     def write(self, tag, attributes='', text=''):
+        """Writes a tag including attributes, of name tag, and containing text."""
         self.__f.write("<{}".format(tag))
         if attributes != '':
             self.__f.write(" {}".format(attributes))
-        self.__f.write(">{}</{}>\n".format(text,tag))
+        self.__f.write(">{}</{}>\n".format(text, tag))
 
-    def writeOrphan(self, tag, attributes):
+    def write_orphan(self, tag, attributes):
+        """Writes an unclosed tag without adding it to the stack."""
         self.__f.write("<{} {}>\n".format(tag, attributes))
 
-    def writeText(self,text):
+    def write_text(self, text):
+        """Writes the supplied text directly."""
         self.__f.write(text)
 
-    def open(self,tag,attributes=''):
-        if attributes=='':
+    def open(self, tag, attributes=''):
+        """Opens a new nested tag."""
+        if attributes == '':
             self.__f.write("<{}>\n".format(tag))
         else:
-            self.__f.write("<{} {}>\n".format(tag,attributes))
+            self.__f.write("<{} {}>\n".format(tag, attributes))
         self.__stack.append("</{}>\n".format(tag))
 
-    def close(self,count=1):
-        for i in range(count): #@UnusedVariable
+    def close(self, count=1):
+        """Closes count number of open tags."""
+        for _ in range(count): #@UnusedVariable
             self.__f.write(self.__stack.pop())
 
     def depth(self):
+        """Returns the number of currenly open tags."""
         return len(self.__stack)
