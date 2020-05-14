551a88b2529cbfaefa32f21720f1eb09a456c526
Jody Sankey <jody@jsankey.com>
2020-05-13 23:49:22 -0700

    Linting of the classifydir module.

--- src/classifydir.py
+++ src/classifydir.py
@@ -1,11 +1,8 @@
-#========================================================
-# ClassifyDir.py
+"""Class to build a hierarchy of directories with information about
+how they should be backed up, as determined by .classify files."""
+
 #========================================================
 # PublicPermissions: True
-#========================================================
-# Class to build a hierarchy of directories with
-# information about how they should be backed up,
-# as determined by .classify files
 #========================================================
 
 import base64
@@ -40,6 +37,7 @@ REQUIRED_SETTINGS = {
 }
 OPTIONAL_SETTINGS = ['name']
 
+# Used by dependant modules.
 VOLUMES = [entry[0] for entry in REQUIRED_SETTINGS['volume'] if entry[0] != 'none']
 PROTECTIONS = [entry[0] for entry in REQUIRED_SETTINGS['protection']]
 
@@ -54,7 +52,6 @@ class ClassifiedDir(object):
     directory, as declared by magic .classify files."""
 
     def __init__(self, base_path, fetch_info, max_recursion_depth=999, rel_path=None, parent=None):
-        """initialize a new ClassifiedDir object"""
         if rel_path is None:
             self.full_path = os.path.abspath(base_path)
             base_path, self.rel_path = os.path.split(self.full_path)
@@ -65,7 +62,7 @@ class ClassifiedDir(object):
             self.base_name = os.path.basename(rel_path)
 
         self.parent = parent
-        self.depth = parent.depth + 1 if parent else 0
+        self.depth = (parent.depth + 1) if parent else 0
         self.recursion_depth = 0
         self.deepest_explicit = -1
         self.children = []
@@ -74,16 +71,16 @@ class ClassifiedDir(object):
         self.last_change = 0 if fetch_info else None
         self.content_hash = b'' if fetch_info else None
 
-        # detect and read the magic file if appropriate
+        # Detect and read the magic file if appropriate
         config_file = os.path.join(self.full_path, MAGIC_FILE)
         if os.path.isfile(config_file):
-            self.__status = 'explicit'
-            self.__readConfigurationFile(config_file)
-            self.__propogateDeepestExplicit()
+            self._status = 'explicit'
+            self._read_configuration_file(config_file)
+            self._propogate_deepest_explicit()
         else:
             self.name = self.base_name
-            if not parent or parent.__status == 'undefined':
-                self.__status = 'undefined'
+            if not parent or parent._status == 'undefined':
+                self._status = 'undefined'
                 self.volume = None
                 self.protection = None
                 self.compress = False
@@ -93,7 +90,7 @@ class ClassifiedDir(object):
                                 "non-recursive classified directory " +
                                 "but does not contain classification file")
             else:
-                self.__status = 'implicit'
+                self._status = 'implicit'
                 self.volume = parent.volume
                 self.protection = parent.protection
                 self.compress = parent.compress
@@ -101,10 +98,10 @@ class ClassifiedDir(object):
                 if parent.recurse:
                     self.recursion_depth = parent.recursion_depth + 1
         if (not self.recurse) or fetch_info or self.recursion_depth < max_recursion_depth:
-            self.__readContents(base_path, fetch_info, max_recursion_depth)
+            self._read_contents(base_path, fetch_info, max_recursion_depth)
 
     def descendants(self):
-        """generator function for all descendant or self classifydir objects."""
+        """Generator function for all descendant or self classifydir objects."""
         # Stack is a list of remaining node lists for each level in a DFS
         to_visit = [self]
         while to_visit:
@@ -112,88 +109,81 @@ class ClassifiedDir(object):
             yield node
             to_visit[:0] = node.children
 
-    def descendantRoots(self):
-        """generator function for descendant or self classifydirs that are an archive root."""
+    def descendant_roots(self):
+        """Generator function for descendant or self classifydirs that are an archive root."""
         for decendent in self.descendants():
-            if decendent.isArchiveRoot():
+            if decendent.is_archive_root():
                 yield decendent
 
-    def descendantMembers(self):
-        """generator function for descendant or self classifydirs inside the current archive. This
+    def descendant_members(self):
+        """Generator function for descendant or self classifydirs inside the current archive. This
         method may only be called on an archive root."""
-        if not self.isArchiveRoot():
+        if not self.is_archive_root():
             raise Exception(self.base_name + ' is not an archive root')
         for descendant in self.descendants():
             if descendant.archiveRoot() is self:
                 yield descendant
 
-    def totalSize(self):
-        """return total size of directory and all children"""
-        if self.size == None:
-            return None
-        else:
-            return sum((cd.size for cd in self.descendants()))
+    def total_size(self):
+        """Return total size of directory and all children"""
+        return sum((cd.size for cd in self.descendants())) if self.size else None
 
-    def totalFileCount(self):
-        """return total number of files in directory and all children"""
-        if self.file_count is None:
-            return None
-        else:
-            return sum((cd.file_count for cd in self.descendants()))
+    def total_file_count(self):
+        """Return total number of files in the directory and all children"""
+        return sum((cd.file_count for cd in self.descendants())) if self.file_count else None
 
-    def archiveRoot(self):
-        """return the classified dir at the root of this archive, or none if not archived"""
-        if self.__status == 'undefined' or self.volume == 'none':
+    def archive_root(self):
+        """Return the classified dir at the root of this archive, or None if not archived"""
+        if self._status == 'undefined' or self.volume == 'none':
             return None
-        elif self.__status == 'implicit':
-            return self.parent.archiveRoot()
+        elif self._status == 'implicit':
+            return self.parent.archive_root()
         else:
             return self
 
-    def isArchiveRoot(self):
-        """return true iff this directory is the root of an archive"""
-        return self.__status == 'explicit' and self.volume != 'none'
+    def is_archive_root(self):
+        """Return true iff this directory is the root of an archive."""
+        return self._status == 'explicit' and self.volume != 'none'
 
-    def archiveSize(self):
-        """return total size of files in an archive when called on the root"""
-        return sum(cd.size for cd in self.descendantMembers())
+    def archive_size(self):
+        """Return total size of files in an archive when called on the root."""
+        return sum(cd.size for cd in self.descendant_members())
 
-    def archiveFileCount(self):
-        """return total number of files in an archive when called on the root"""
-        return sum(cd.file_count for cd in self.descendantMembers())
+    def archive_file_count(self):
+        """Return total number of files in an archive when called on the root."""
+        return sum(cd.file_count for cd in self.descendant_members())
 
-    def archiveLastChange(self):
-        """return greatest file modification time in an archive when called on the root"""
-        return max(cd.last_change for cd in self.descendantMembers())
+    def archive_last_change(self):
+        """Return greatest file modification time in an archive when called on the root."""
+        return max(cd.last_change for cd in self.descendant_members())
 
-    def archiveHash(self):
-        """return a string hash of file state when called on the root"""
+    def archive_hash(self):
+        """Return a string hash of file state when called on the root."""
         hasher = hashlib.md5()
-        for cd in self.descendantMembers():
-            hasher.update(cd.content_hash)
+        for desc in self.descendant_members():
+            hasher.update(desc.content_hash)
         return base64.urlsafe_b64encode(hasher.digest()[:6]).decode('utf-8')
 
-    def archiveFilenames(self):
-        """generator for all filenames within an archive when called on the root"""
-        for cd in self.descendantMembers():
-            files = next(os.walk(cd.full_path, topdown=True, followlinks=False))[2]
+    def archive_filenames(self):
+        """Generator for all filenames within an archive when called on the root."""
+        for desc in self.descendant_members():
+            files = next(os.walk(desc.full_path, topdown=True, followlinks=False))[2]
             for entry in sorted(files):
-                entry_path = os.path.join(cd.full_path, entry)
+                entry_path = os.path.join(desc.full_path, entry)
                 status = os.lstat(entry_path)
                 if stat.S_ISREG(status.st_mode):
                     yield entry_path
 
-    def volumeColor(self):
-        """Returns an Xterminal control string for a color reflecting required volume"""
+    def volume_color(self):
+        """Returns an Xterminal control string for a color reflecting required volume."""
         return VOLUME_COLORS[self.volume]
 
-    def protectionColor(self):
-        """Returns an Xterminal control string for a color reflecting required protection"""
+    def protection_color(self):
+        """Returns an Xterminal control string for a color reflecting required protection."""
         return PROTECTION_COLORS[self.protection]
 
-
-    def __readContents(self, base_path, fetch_info, max_recursion_depth):
-        """"Adds child objects and optionally sizes based on directories inside our own"""
+    def _read_contents(self, base_path, fetch_info, max_recursion_depth):
+        """"Adds child objects and optionally sizes based on directories inside our own."""
         try:
             dirs, files = next(os.walk(self.full_path, topdown=True, followlinks=False))[1:]
         except StopIteration:
@@ -217,16 +207,15 @@ class ClassifiedDir(object):
                     hasher.update(int(status.st_size).to_bytes(8, byteorder='little'))
             self.content_hash = hasher.digest()
 
-
-    def __readConfigurationFile(self, file_path):
+    def _read_configuration_file(self, file_path):
         """Adds values read from the configuration file at file_path. Legal
         settings are defined by REQUIRED_SETTINGS and OPTIONAL_SETTINGS."""
         f = open(file_path, 'r')
         try:
-            for (tag, value) in [_parseLine(l) for l in f if _parseLine(l)]:
-                self.__validateSetting(tag, value)
-                setattr(self, tag, _stringBool(value))
-            for setting in REQUIRED_SETTINGS.keys():
+            for (tag, value) in [_parse_line(l) for l in f if _parse_line(l)]:
+                self._validate_setting(tag, value)
+                setattr(self, tag, _string_bool(value))
+            for setting in REQUIRED_SETTINGS:
                 if not hasattr(self, setting):
                     raise Exception("{} not specified".format(setting))
             if not hasattr(self, 'name'):
@@ -236,14 +225,14 @@ class ClassifiedDir(object):
         finally:
             f.close()
 
-    def __propogateDeepestExplicit(self):
+    def _propogate_deepest_explicit(self):
         """Propogates the current depth through all ancestors with a lower value."""
         node = self
         while node and node.deepest_explicit < self.depth:
             node.deepest_explicit = self.depth
             node = node.parent
 
-    def __validateSetting(self, tag, value):
+    def _validate_setting(self, tag, value):
         """Ensures the specified tag value pair is legal. Throws an exception if not."""
         if tag in REQUIRED_SETTINGS.keys():
             if value not in [vd[0] for vd in REQUIRED_SETTINGS[tag]]:
@@ -255,7 +244,7 @@ class ClassifiedDir(object):
 
 
 
-def _parseLine(line):
+def _parse_line(line):
     """If line is in the form Tag=value[#Comment] returns a (tag, value)
     tuple, otherwise returns None"""
     non_comment = line.split('#')[0].strip()
@@ -267,7 +256,7 @@ def _parseLine(line):
     else:
         return None
 
-def _stringBool(value):
+def _string_bool(value):
     """Returns a matching boolean if the input is true or false, else no change."""
     if value == 'true': return True
     if value == 'false': return False
