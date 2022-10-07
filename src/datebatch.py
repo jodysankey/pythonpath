aca5d943d348d14b619a0a730247f17996924b1e
Jody Sankey <jody@jsankey.com>
2022-10-07 01:04:58 -0700

    Modernize datebatch

--- src/datebatch.py
+++ src/datebatch.py
@@ -16,121 +16,108 @@ _FAIL_TEXT = "FAIL   "
 _SKIP_TEXT = " -     "
 
 
-class DateBatcher(object):
+class DateBatcher:
     """A class to perform the same function as the DateBatch executable.
 
     Initialize with either setUsingDir or setUsingLog before use
     or all other functions will fail."""
 
-    def __init__(self):
-        """Initialize all attributes to a standard default"""
-        self.spacing = 0
-        self.count = 0
-        self.log = ""
-        self.dir = ""
-        self.dirMode = True
-        self.function = None
+    def __init__(self, output_dir, spacing, count, dir_mode, function, log_path):
+        """Initialize all attributes to the supplied inputs
 
-
-
-    def setUsingDir(self, dir, spacing, count, function, log=""):
-        """Initialize class to create dated subdirectories within dir.
-
-        Dir      -- Parent directory for dated subdirectories
-        Spacing  -- Minimum number of days between runs
-        Count    -- Number of runs to maintain
-        Function -- Python function called with subdirectory to perform run
-        Log      -- Optional log file to document run attempts
-        """
+        output_dir   -- Parent directory for dated subdirectories, or directory passed
+                        directly to the function in log mode.
+        spacing      -- Minimum number of days between runs.
+        count        -- Number of runs to maintain in directory mode.
+        function     -- Python function called with subdirectory to perform run.
+        log_path     -- Optional log file to document run attempts."""
+        self.output_dir = output_dir
         self.spacing = spacing
         self.count = count
-        self.dir = dir
-        self.log = log
+        self.dir_mode = dir_mode
         self.function = function
-        self.dirMode = True
+        self.log_path = log_path
 
-    def setUsingLog(self, dir, spacing, function, log):
-        """Initialize class to execute based on runs in a log.
+    @staticmethod
+    def using_dir(output_dir, spacing, count, function, log_path=None):
+        """Initialize class to create dated subdirectories within ouput_dir."""
+        return DateBatcher(output_dir=output_dir,
+                           spacing=spacing,
+                           count=count,
+                           dir_mode=True,
+                           function=function,
+                           log_path=log_path)
 
-        Dir      -- Directory passed to function
-        Spacing  -- Minimum number of days between runs
-        Function -- Python function called with directory to perform run
-        Log      -- Log file used to track when runs are performed
-        """
-        self.spacing = spacing
-        self.count = 1
-        self.dir = dir
-        self.log = log
-        self.function = function
-        self.dirMode = False
-
+    @staticmethod
+    def using_log(output_dir, spacing, function, log_path):
+        """Initialize class to execute based on runs in a log."""
+        return DateBatcher(output_dir=output_dir,
+                           spacing=spacing,
+                           count=1,
+                           dir_mode=False,
+                           function=function,
+                           log_path=log_path)
 
     def validate(self):
         """Throw an exception if the instance variables are not valid"""
         if self.count < 1 or self.spacing < 1:
             raise AttributeError
-        if len(self.dir) < 1:
+        if not self.output_dir:
             raise AttributeError
         if not self.function or not hasattr(self.function, '__call__'):
             raise AttributeError
 
 
-    def lastSuccess(self):
+    def last_success(self):
         """Return the date of the last successful run, or None otherwise"""
         self.validate()
         last_date = None
 
-        if self.dirMode:
-            dated_dirs = self.__datedDirs()
+        if self.dir_mode:
+            dated_dirs = self.__dated_dirs()
             if len(dated_dirs) > 0:
                 return dated_dirs[0]
-        elif os.path.isfile(self.log):
-            for line in open(self.log, "r"):
-                if line.startswith((_GOOD_TEXT, _FORCE_TEXT)):
-                    match = re.search(r"(\d{4})-(\d{2})-(\d{2})", line)
-                    if match is not None:
-                        last_date = date(*[int(x) for x in match.groups()])
-
+        elif os.path.isfile(self.log_path):
+            with open(self.log_path, "r") as log_file:
+                for line in log_file:
+                    if line.startswith((_GOOD_TEXT, _FORCE_TEXT)):
+                        match = re.search(r"(\d{4})-(\d{2})-(\d{2})", line)
+                        if match is not None:
+                            last_date = date(*[int(x) for x in match.groups()])
         return last_date
 
-
-    def runRequired(self):
+    def run_required(self):
         """Return True if a run is required, or False if not"""
         self.validate()
-        last_success = self.lastSuccess()
+        last_success = self.last_success()
         today = date.today()
         return last_success is None or (today - last_success).days >= self.spacing
 
-
-    def removeExcessDirectories(self):
+    def remove_excess_directories(self):
         """Remove the oldest subdirectories while count is above the max"""
-        if not self.dirMode: raise ValueError
-        dated_dirs = self.__datedDirs()
+        if not self.dir_mode:
+            raise ValueError
+        dated_dirs = self.__dated_dirs()
         while len(dated_dirs) > self.count:
-            shutil.rmtree(self.dir + "/" + dated_dirs.pop().isoformat())
+            shutil.rmtree(os.path.join(self.output_dir, dated_dirs.pop().isoformat()))
 
-
-    def forceExecute(self):
+    def force_execute(self):
         """Force the run to be performed even if not required"""
         self.__execute(True)
-
 
     def execute(self):
         """Perform the run, but only if required"""
         self.__execute(False)
 
-        if len(self.dir) < 1:
+        if len(self.output_dir) < 1:
             raise AttributeError
 
-
-
-
-    def __datedDirs(self):
+    def __dated_dirs(self):
         """Returns an ordered list of all date subdirectories, latest first"""
-        assert self.dirMode
+        assert self.dir_mode
 
         dated_dirs = []
-        for string in os.listdir(self.dir):
+        for string in os.listdir(self.output_dir):
             match = re.search(r"(\d{4})-(\d{2})-(\d{2})", string)
             if match is not None:
                 dated_dirs.append(date(*[int(x) for x in match.groups()]))
@@ -139,49 +126,48 @@ class DateBatcher(object):
         return dated_dirs
 
 
-    def __addLogEntry(self, summary, reason=None):
+    def __add_log_entry(self, summary, reason=None):
         """Add an line starting with summary to the log, if it was set"""
-        if self.log:
-            log_file = open(self.log, "a+")
-            line = summary + datetime.now().isoformat()
-            if reason:
-                line += " ("+reason+")"
-            log_file.write(line+"\n")
-            log_file.close()
+        if self.log_path:
+            with open(self.log_path, "a+") as log_file:
+                line = summary + datetime.now().isoformat()
+                if reason:
+                    line += " ("+reason+")"
+                log_file.write(line+"\n")
 
 
     def __execute(self, force):
         """Perform the mechanics of a run, if required or if told to force"""
         self.validate()
-        if not force and not self.runRequired():
-            self.__addLogEntry(_SKIP_TEXT)
+        if not force and not self.run_required():
+            self.__add_log_entry(_SKIP_TEXT)
             return
 
         today = date.today()
         adding_dir = False
-        target = self.dir
+        target = self.output_dir
 
-        if self.dirMode:
+        if self.dir_mode:
             target += "/" + today.isoformat()
-            dated_dirs = self.__datedDirs()
+            dated_dirs = self.__dated_dirs()
             # Create today's directory if necessary
             if today not in dated_dirs:
                 os.mkdir(target)
                 adding_dir = True
 
             # Now if we've reached the count limit, delete the oldest directory
-            self.removeExcessDirectories()
+            self.remove_excess_directories()
 
         # Run the command, trapping errors and removing the directory if
         # we just created it but the function failed
         try:
             self.function(target)
         except Exception as ex:
-            self.__addLogEntry(_FAIL_TEXT, str(ex))
+            self.__add_log_entry(_FAIL_TEXT, str(ex))
             if adding_dir:
                 shutil.rmtree(target)
         else:
             if force:
-                self.__addLogEntry(_FORCE_TEXT)
+                self.__add_log_entry(_FORCE_TEXT)
             else:
-                self.__addLogEntry(_GOOD_TEXT)
+                self.__add_log_entry(_GOOD_TEXT)
--- tests/test_datebatch.py
+++ tests/test_datebatch.py
@@ -1,145 +1,136 @@
+#!/usr/bin/python3
+
 from datetime import date, datetime, timedelta
-import datebatch
 import os
-import shutil
+import tempfile
 import unittest
 
+import datebatch
 
-#test_path = os.getenv("TMP") + "/DateBatchTest" # TMP set to some hideous directory
-test_path = r"C:\Temp\DateBatchTest"
-test_file = "test.txt"
-test_log = test_path + "/test.log"
+TEST_FILE = "test.txt"
 
-
-def testFunction(dir):
+def test_function(root_dir):
     """Function suitable for datebatch use. Adds a date line to test_file"""
-    exclusion_file = open(dir + "/" + test_file,"a+")
+    exclusion_file = open(root_dir + "/" + TEST_FILE, "a+")
     exclusion_file.write("Ran test function " + datetime.now().isoformat() + "\n")
     exclusion_file.close()
 
-def exceptionFunction(dir):
+def exception_function(root_dir):
     """Function suitable for datebatch use. Throws an exception"""
     raise AttributeError
 
 
 class DateBatchTestCase(unittest.TestCase):
+    """Tests for the DateBatcher class."""
 
     def setUp(self):
-        #Make sure we remove any existing test dir first, then create a new one
-        try:
-            os.stat(test_path)
-        except WindowsError:
-            pass
-        else:
-            shutil.rmtree(test_path)
-        os.mkdir(test_path)
-        
-    def testInitializationRequirement(self):
-        db = datebatch.DateBatcher()
-        self.assertRaises(AttributeError,db.runRequired)
-        self.assertRaises(AttributeError,db.forceExecute)
-        self.assertRaises(AttributeError,db.execute)
+        self.output_dir = tempfile.TemporaryDirectory()
+        self.output_path = self.output_dir.name
+        self.log_path = os.path.join(self.output_path, "test.log")
+        self.today = date.today()
 
-    def testFunctionException(self):
-        db = datebatch.DateBatcher()
-        db.setUsingDir(test_path, 1, 1, exceptionFunction)
-        db.execute()
-        self.failUnless(db.runRequired(),"Did not require rerun following failure")
+    def tearDown(self):
+        self.output_dir.cleanup()
+        return super().tearDown()
+
+    def date_string(self, delta_days):
+        """Returns a ISO date string the specified number of days after today."""
+        return (self.today + timedelta(delta_days)).isoformat()
+
+    def test_initialization_requirement(self):
+        date_batcher = datebatch.DateBatcher(None, 1, 1, None, None, None)
+        self.assertRaises(AttributeError, date_batcher.run_required)
+        self.assertRaises(AttributeError, date_batcher.force_execute)
+        self.assertRaises(AttributeError, date_batcher.execute)
+
+    def test_function_exception(self):
+        date_batcher = datebatch.DateBatcher.using_dir(self.output_path, 1, 1, exception_function)
+        date_batcher.execute()
+        self.assertTrue(date_batcher.run_required(), "Did not require rerun following failure")
+
+    def test_dir_based_without_log(self):
+        # Init object
+        date_batcher = datebatch.DateBatcher.using_dir(self.output_path, 3, 2, test_function)
+
+        # Test run required returns appropriate values
+        req = date_batcher.run_required()
+        self.assertTrue(req, "Did not require run on empty directory")
+        os.mkdir(os.path.join(self.output_path, self.date_string(-3)))
+        req = date_batcher.run_required()
+        self.assertTrue(req, "Did not require run on out of date directory")
+        date_batcher = datebatch.DateBatcher.using_dir(self.output_path, 4, 2, test_function)
+        req = date_batcher.run_required()
+        self.assertFalse(req, "Required run on in-date directory")
+
+        # Test with log continues to test more complex conditions
 
 
-    def testDirBasedWithoutLog(self):
-        #Init object
-        db = datebatch.DateBatcher()
-        today = date.today()
-        db.setUsingDir(test_path, 3, 2, testFunction)
+    def test_dir_based_with_log(self):
+        # Init object
+        date_batcher = datebatch.DateBatcher.using_dir(
+            self.output_path, 3, 2, test_function, self.log_path)
 
-        #Test run required returns appropriate values
-        req = db.runRequired()
-        self.failUnless(req,"Did not require run on empty directory")
-        os.mkdir(test_path + "/" + (today-timedelta(3)).isoformat())
-        req = db.runRequired()
-        self.failUnless(req,"Did not require run on out of date directory")
-        db.setUsingDir(test_path, 4, 2, testFunction)
-        req = db.runRequired()
-        self.failIf(req,"Required run on in-date directory")
+        # Test run required returns appropriate values
+        req = date_batcher.run_required()
+        self.assertTrue(req, "Did not require run on empty directory")
+        os.mkdir(os.path.join(self.output_path, self.date_string(-3)))
+        req = date_batcher.run_required()
+        self.assertTrue(req, "Did not require run on out of date directory")
+        date_batcher.spacing = 5
+        req = date_batcher.run_required()
+        self.assertFalse(req, "Required run on in-date directory")
 
-        #Test with log continues to test more complex conditions
-
-
-    def testDirBasedWithLog(self):
-        #Init object
-        db = datebatch.DateBatcher()
-        today = date.today()
-        db.setUsingDir(test_path, 3, 2, testFunction, test_log)
-
-        #Test run required returns appropriate values
-        req = db.runRequired()
-        self.failUnless(req,"Did not require run on empty directory")
-        os.mkdir(test_path + "/" + (today-timedelta(3)).isoformat())
-        req = db.runRequired()
-        self.failUnless(req,"Did not require run on out of date directory")
-        db.setUsingDir(test_path, 4, 2, testFunction, test_log)
-        req = db.runRequired()
-        self.failIf(req,"Required run on in-date directory")
-
-        #Test run is executed when, and only when, it is required
-        test_full_file = test_path+"/"+today.isoformat()+"/"+test_file
-        db.setUsingDir(test_path, 3, 2, testFunction, test_log)
-        db.execute()
-        self.failUnless(os.path.exists(test_full_file),
-            "Command did not execute first time")
+        # Test run is executed when, and only when, it is required
+        date_batcher.spacing = 3
+        test_full_file = os.path.join(self.output_path, self.date_string(0), TEST_FILE)
+        date_batcher.execute()
+        self.assertTrue(os.path.exists(test_full_file),
+                        "Command did not execute first time")
         sz1 = os.stat(test_full_file).st_size
-        db.execute()
+        date_batcher.execute()
         sz2 = os.stat(test_full_file).st_size
-        self.failIf(sz2>sz1,"Command reexecuted second time")
-        db.forceExecute()
+        self.assertFalse(sz2 > sz1, "Command reexecuted second time")
+        date_batcher.force_execute()
         sz3 = os.stat(test_full_file).st_size
-        self.failUnless(sz3>sz1,"Command did not force reexecute")
+        self.assertTrue(sz3 > sz1, "Command did not force reexecute")
 
-        #Test run deletes excess directories when, and only when, required
-        os.mkdir(test_path + "/" + (today-timedelta(8)).isoformat())
-        db.forceExecute()
-        self.failIf(os.path.exists(test_path+"/"+(today-
-            timedelta(8)).isoformat()),"Did not delete outdated directory")
-        self.failUnless(os.path.exists(test_path+"/"+(today-
-            timedelta(3)).isoformat()),"Deleted in-date directory")
+        # Test run deletes excess directories when, and only when, required
+        os.mkdir(os.path.join(self.output_path, self.date_string(-8)))
+        date_batcher.force_execute()
+        self.assertFalse(os.path.exists(os.path.join(self.output_path, self.date_string(-8))),
+                         "Did not delete outdated directory")
+        self.assertTrue(os.path.exists(os.path.join(self.output_path, self.date_string(-3))),
+                        "Deleted in-date directory")
 
-        #Test log was actually created
-        self.failUnless(os.path.exists(test_log), "Did not create log")
+        # Test log was actually created
+        self.assertTrue(os.path.exists(self.log_path), "Did not create log")
 
 
-    def testLogBased(self):
-        #Init object
-        db = datebatch.DateBatcher()
-        db.setUsingLog(test_path, 3, exceptionFunction, test_log)
+    def test_log_based(self):
+        # Init object
+        date_batcher = datebatch.DateBatcher.using_log(
+            self.output_path, 3, exception_function, self.log_path)
 
-        #Test run only runs when required        
-        req = db.runRequired()
-        self.failUnless(req,"Did not require run on clean directory")
-        db.execute()
-        req = db.runRequired()
-        self.failUnless(req,"Did not require rerun after failure")
-        db.setUsingLog(test_path, 3, testFunction, test_log)
-        db.execute()
-        req = db.runRequired()
-        self.failIf(req,"Still required run after success")
+        # Test run only runs when required
+        self.assertTrue(date_batcher.run_required(), "Did not require run on clean directory")
+        date_batcher.execute()
+        self.assertTrue(date_batcher.run_required(), "Did not require rerun after failure")
+        date_batcher.function = test_function
+        date_batcher.execute()
+        self.assertFalse(date_batcher.run_required(), "Still required run after success")
 
-        #Test run is executed when, and only when, it is required
-        test_full_file = test_path+"/"+test_file
-        self.failUnless(os.path.exists(test_full_file),
-            "Command did not execute first time")
+        # Test run is executed when, and only when, it is required
+        test_full_file = os.path.join(self.output_path, TEST_FILE)
+        self.assertTrue(os.path.exists(test_full_file),
+                        "Command did not execute first time")
         sz1 = os.stat(test_full_file).st_size
-        db.execute()
+        date_batcher.execute()
         sz2 = os.stat(test_full_file).st_size
-        self.failIf(sz2>sz1,"Command reexecuted second time")
-        db.forceExecute()
+        self.assertFalse(sz2 > sz1, "Command reexecuted second time")
+        date_batcher.force_execute()
         sz3 = os.stat(test_full_file).st_size
-        self.failUnless(sz3>sz1,"Command did not force reexecute")
+        self.assertTrue(sz3 > sz1, "Command did not force reexecute")
 
 
-if __name__ == "__main__": unittest.main()
-
-
-
-
-#testLogMode
+if __name__ == "__main__":
+    unittest.main()
